f98e314609e6e7bb083c0ad072b795309acdbe9d
Haleotik <101292395+Haleotik@users.noreply.github.com>
2025-06-05 21:42:57 +0300

    4

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,22 +5,55 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject spawner_1;
-    GameObject inst_cont;
-    public GameObject copied_obj;
+    public float _timer;
+    
+    GameObject inst_cont;    
+    GameObject copied_obj;
+
+    public GameObject[] GOArray;
+    private int _chosen;
+    public int _quantity = 1;
+    
     // Start is called before the first frame update
     void Start()
     {
-        inst_cont = Instantiate(copied_obj, spawner_1.transform.position, spawner_1.transform.rotation);
-        inst_cont.transform.SetParent(spawner_1.transform, true);
+        //inst_cont = Instantiate(copied_obj, spawner_1.transform.position, spawner_1.transform.rotation);
         
-        //spawner_1.
+        //inst_cont.transform.SetParent(gameObject.transform, true);
+        
+        StartCoroutine(cust_coroutine());
 
-
+        Debug.Log(GOArray.Length);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    IEnumerator cust_coroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (_chosen == GOArray.Length - 1)
+        {
+            _chosen = 0;
+        }
+        else
+        { _chosen = _chosen + 1; }
+
+        copied_obj = GOArray[_chosen];
+        inst_cont = Instantiate(copied_obj, spawner_1.transform.position, spawner_1.transform.rotation);
+        inst_cont.transform.SetParent(gameObject.transform, true);
+        if (_quantity < 6)
+        {
+            StartCoroutine(cust_coroutine());
+            _quantity += 1;
+        }
         
     }
+
+    
+
 }
